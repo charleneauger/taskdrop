5336d119927552f1d6471bc4f93956ed9101803a
Charlene Auger <charlene.auger@factorfx.com>
2022-05-05 15:46:15 +0000

    Add calendar.js

---
+++ js/calendar.js
@@ -0,0 +1,43 @@
+var modalWindow;
+
+$(function() {
+   modalWindow = $("<div></div>").dialog({
+      resizable: false,
+      width: '200',
+      autoOpen: false,
+      height: '150',
+      modal: true,
+      position: {
+         my: 'center'
+      },
+      open: function( event, ui ) {
+         //remove existing tinymce when reopen modal (without this, tinymce don't load on 2nd opening of dialog)
+         modalWindow.find('.mce-container').remove();
+      }
+   });
+ });
+
+ var plugin_Taskdrop = new function() {
+
+   this.modalSettings = {
+      resizable: false,
+      width: '200',
+      autoOpen: false,
+      height: '150',
+      modal: true,
+      position: {
+         my: 'center'
+      },
+      close: function() {
+         $(this).dialog('close');
+         $(this).remove();
+      }
+   }
+
+   this.showTicketStatus = function () {
+      modalWindow.load(
+         '/glpi/plugins/taskdrop/ajax/planning.php?ticket_status=update_ticket_status'
+      ).dialog('open');
+   }
+
+}
